e8f61996df2d818ca4301c43fd6982b5f129835a
Maksim Pisartsev <Maxwell88@yandex.ru>
2022-07-13 18:45:55 +0300

    fix: fix HTML validation errors and image paths

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,27 @@
 const initialCards = [
   {
     name: 'Сочи',
-    link: '../images/dima-fedorov-sochi.jpg',
+    link: './images/dima-fedorov-sochi.jpg',
   },
   {
     name: 'Ялта',
-    link: '../images/dmitry-bogatyrev-yalta.jpg',
+    link: './images/dmitry-bogatyrev-yalta.jpg',
   },
   {
     name: 'Домбай',
-    link: '../images/kirill-pershin-dombai.jpg',
+    link: './images/kirill-pershin-dombai.jpg',
   },
   {
     name: 'Эльбрус',
-    link: '../images/kirill-pershin-elbrus.jpg',
+    link: './images/kirill-pershin-elbrus.jpg',
   },
   {
     name: 'Карачаево-Черкессия',
-    link: '../images/kirill-pershin-karachaevsk.jpg',
+    link: './images/kirill-pershin-karachaevsk.jpg',
   },
   {
     name: 'Алтай',
-    link: '../images/sergei-wing-altay.jpg',
+    link: './images/sergei-wing-altay.jpg',
   },
 ];
 
