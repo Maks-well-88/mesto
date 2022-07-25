44e26b365c3678e85d3313a2d440a0ccf646e27c
Maksim Pisartsev <Maxwell88@yandex.ru>
2022-07-26 00:51:25 +0300

    feat: add validation forms

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,13 +1,40 @@
-// данные любого поля ввода проверяются одной унифицированной функцией;
-// для проверки данных в поле используются HTML5-атрибуты и JS-свойство ValidityState ;
-// за состояние кнопки сабмита отвечает отдельная функция;
-// функция enableValidation, которая включает валидацию, принимает на вход объект параметров, а затем передаёт параметры вложенным функциям;
-// появление сообщения об ошибке в одну или две строки высотой не должно приводить к изменению высоты модального окна. Если сообщение об ошибке от трёх строк высотой, то размер модального окна увеличивается.
-// ({
-//   formSelector: '.popup__form',
-//   inputSelector: '.popup__input',
-//   submitButtonSelector: '.popup__button',
-//   inactiveButtonClass: 'popup__button_disabled',
-//   inputErrorClass: 'popup__input_type_error',
-//   errorClass: 'popup__error_visible',
-// });
+const object = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button-save',
+  inactiveButtonClass: 'popup__button-save_disabled',
+  inputErrorClass: 'popup__input-field_type_error',
+};
+
+const enableValidation = (object) => {
+  const forms = document.querySelectorAll(object.formSelector);
+  forms.forEach((form) => form.addEventListener('input', handleInputValidation));
+
+  function handleInputValidation(event) {
+    const input = event.target;
+    const form = input.closest(object.formSelector);
+    showInputError(input);
+    activateSubmitButton(form);
+  }
+
+  function showInputError(input) {
+    const valid = input.validity.valid;
+    const errorField = input.nextElementSibling;
+    errorField.textContent = input.validationMessage;
+    !valid ? input.classList.add(object.inputErrorClass) : input.classList.remove(object.inputErrorClass);
+  }
+
+  function activateSubmitButton(form) {
+    const button = form.querySelector(object.submitButtonSelector);
+
+    if (form.checkValidity()) {
+      button.removeAttribute('disabled');
+      button.classList.remove(object.inactiveButtonClass);
+    } else {
+      button.setAttribute('disabled', 'true');
+      button.classList.add(object.inactiveButtonClass);
+    }
+  }
+};
+
+enableValidation(object);
