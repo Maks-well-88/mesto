a7b5073388462a0f463b7a46bb8e194c2a9f04a2
Maksim Pisartsev <Maxwell88@yandex.ru>
2022-08-24 13:40:30 +0300

    feat: tune webpack

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import Card from './Card.js';
-import FormValidator from './FormValidator.js';
-import Section from './Section.js';
-import PopupWithImage from './PopupWithImage.js';
-import PopupWithForm from './PopupWithForm.js';
-import UserInfo from './UserInfo.js';
-import { initialCards } from './data.js';
+import './pages/index.css';
+import Card from './scripts/Card.js';
+import FormValidator from './scripts/FormValidator.js';
+import Section from './scripts/Section.js';
+import PopupWithImage from './scripts/PopupWithImage.js';
+import PopupWithForm from './scripts/PopupWithForm.js';
+import UserInfo from './scripts/UserInfo.js';
+import { initialCards } from './scripts/data.js';
 import {
   selectors,
   blockOfElements,
@@ -16,7 +17,7 @@ import {
   profileJob,
   forms,
   validatorsList,
-} from './constants.js';
+} from './scripts/constants.js';
 
 // create validation
 forms.forEach((form) => {
